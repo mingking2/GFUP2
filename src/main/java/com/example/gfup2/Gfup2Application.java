5ce25ece5a2175e423def31b81ea338fc963b1bc
King's meow <mingee641@gmail.com>
2023-11-22 20:07:26 +0900

    Initial commit

---
+++ src/main/java/com/example/gfup2/Gfup2Application.java
@@ -0,0 +1,13 @@
+package com.example.gfup2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gfup2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gfup2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gfup2/Gfup2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gfup2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gfup2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

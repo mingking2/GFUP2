52ea3a77ab938e87088412b0442bbc97134f221b
King's meow <mingee641@gmail.com>
2023-12-07 20:10:45 +0900

    :sparkles: 마이페이지 controller 추가

---
+++ src/main/java/com/example/gfup2/web/user/controller/InfoController.java
@@ -0,0 +1,26 @@
+package com.example.gfup2.web.user.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class InfoController {
+
+    @GetMapping("/profile")
+    public void showProfile() {
+
+    }
+
+    @PostMapping("/profileUpload")
+    public void uploadProfile() {
+
+    }
+
+    @PostMapping("/changePhoneNum")
+    public void changePhoneNumber() {
+
+    }
+}

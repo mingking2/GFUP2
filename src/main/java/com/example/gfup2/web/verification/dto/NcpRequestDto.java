c31a39372b9fd3ef103acff26dec0568e0339207
minju <minju9711@naver.com>
2023-12-12 03:36:35 +0900

    :sparkles: Ncp에게 요청할 dto, 반환 내용 담을 dto 생성

---
+++ src/main/java/com/example/gfup2/web/verification/dto/NcpRequestDto.java
@@ -0,0 +1,18 @@
+package com.example.gfup2.web.verification.dto;
+
+import lombok.*;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Builder
+public class NcpRequestDto {
+    String type;
+    String contentType;
+    String countryCode;
+    String from;
+    String content;
+    List<MessageDto> messages;
+}

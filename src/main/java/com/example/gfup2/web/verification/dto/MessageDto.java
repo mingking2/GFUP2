c0766c558183222872338390b7279a32c0686c0b
minju <minju9711@naver.com>
2023-12-14 18:06:16 +0900

    :art: 필요없는 롬복 삭제

--- src/main/java/com/example/gfup2/web/verification/dto/MessageDto.java
+++ src/main/java/com/example/gfup2/web/verification/dto/MessageDto.java
@@ -2,11 +2,9 @@ package com.example.gfup2.web.verification.dto;
 
 import lombok.*;
 
-@AllArgsConstructor
 @NoArgsConstructor
 @Setter
 @Getter
-@Builder
 public class MessageDto {
     String to;
 }
--- src/main/java/com/example/gfup2/web/verification/dto/NcpResponseDto.java
+++ src/main/java/com/example/gfup2/web/verification/dto/NcpResponseDto.java
@@ -3,11 +3,7 @@ package com.example.gfup2.web.verification.dto;
 import lombok.*;
 import java.time.LocalDateTime;
 
-@AllArgsConstructor
 @NoArgsConstructor
-@Setter
-@Getter
-@Builder
 public class NcpResponseDto {
     String requestId;
     LocalDateTime requestTime;

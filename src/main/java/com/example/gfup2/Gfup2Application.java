559fa69c1f1d86e1bb68f99ef77015671b13d309
King's meow <mingee641@gmail.com>
2023-12-17 17:53:32 +0900

    :package: 알람기능 구현을 위한 스케쥴러 추가

--- src/main/java/com/example/gfup2/Gfup2Application.java
+++ src/main/java/com/example/gfup2/Gfup2Application.java
@@ -2,8 +2,10 @@ package com.example.gfup2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Gfup2Application {
 
     public static void main(String[] args) {

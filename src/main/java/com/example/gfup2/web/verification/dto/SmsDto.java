c0b749e94fbcfcb75ab273708381ebef244f19f1
King's meow <mingee641@gmail.com>
2023-12-26 15:31:07 +0900

    :bug: 설정값 바꾸지마 ㅜㅜ

---
+++ src/main/java/com/example/gfup2/web/verification/dto/SmsDto.java
@@ -0,0 +1,13 @@
+package com.example.gfup2.web.verification.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class SmsDto {
+
+    private String phoneNumber;
+}

40b34803aba5124814087acad2ed5fe5ebac3108
SangJung <sangjung245@gmail.com>
2024-01-22 17:27:48 +0900

    [FEAT] 상수 관리 파일 추가

---
+++ src/main/java/com/example/gfup2/constants/Constants.java
@@ -0,0 +1,7 @@
+package com.example.gfup2.constants;
+
+public class Constants {
+    public static final String REFRESH_TOKEN = "refreshToken";
+    public static final String ACCESS_TOKEN = "accessToken";
+    public static final String TOKEN_TYPE="typ";
+}

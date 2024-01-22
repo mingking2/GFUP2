10a47fffec42f6017d79e9f8c7d64299dbe8ec15
SangJung <sangjung245@gmail.com>
2024-01-22 17:52:00 +0900

    [MODIFY] 생성자 추가 및 유효성 검사 조건 추가

--- src/main/java/com/example/gfup2/dto/VerifyNumberDto.java
+++ src/main/java/com/example/gfup2/dto/VerifyNumberDto.java
@@ -2,15 +2,19 @@ package com.example.gfup2.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
-import lombok.Getter;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Getter
+@Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class VerifyNumberDto {
 
     @NotBlank(message = "숫자를 입력해야 합니다.")
     @Pattern(regexp = "\\d+", message = "숫자만 입력해야 합니다.")
+    @Size(max=30)
     String number;
-
 }
+

ac3f0d3d48b257549de913be254ceb4f4215c01a
SangJung <sangjung245@gmail.com>
2024-01-22 22:25:27 +0900

    [FEAT] 컨트롤러베이스 인터페이스 추가

--- src/main/java/com/example/gfup2/controller/ControllerBase.java
+++ src/main/java/com/example/gfup2/controller/ControllerBase.java
@@ -1,10 +1,9 @@
 package com.example.gfup2.controller;
 
-import com.example.gfup2.exception.EntityException;
-import com.example.gfup2.exception.ValidException;
-import com.example.gfup2.exception.VerifyException;
 import org.springframework.http.ResponseEntity;
 
+import javax.naming.Binding;
+
 public interface ControllerBase {
-    public ResponseEntity<?> run() throws ValidException, VerifyException, EntityException;
+    public ResponseEntity<?> run() throws Exception;
 }

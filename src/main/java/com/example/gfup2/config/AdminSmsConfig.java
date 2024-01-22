e01830b396538248005954fe87998dc5f2924cbe
SangJung <sangjung245@gmail.com>
2024-01-22 17:19:04 +0900

    [DEPRECATED] sms 설정 파일 비활성화

--- src/main/java/com/example/gfup2/config/AdminSmsConfig.java
+++ src/main/java/com/example/gfup2/config/AdminSmsConfig.java
@@ -5,10 +5,11 @@ import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-@Getter
-@Setter
-@Configuration
-@ConfigurationProperties(prefix = "sms")
+//@Getter
+//@Setter
+//@Configuration
+//@ConfigurationProperties(prefix = "sms")
+@Deprecated
 public class AdminSmsConfig {
 
     private String accessKey;

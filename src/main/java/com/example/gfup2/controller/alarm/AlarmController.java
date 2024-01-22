2308e357f00beee16c9ade42c4fdf52f2446dff3
SangJung <sangjung245@gmail.com>
2024-01-22 22:04:10 +0900

    [REFACTOR] 구현부 람다식으로 변경

--- src/main/java/com/example/gfup2/controller/alarm/AlarmController.java
+++ src/main/java/com/example/gfup2/controller/alarm/AlarmController.java
@@ -1,5 +1,7 @@
 package com.example.gfup2.controller.alarm;
 
+import com.example.gfup2.controller.ControllerBase;
+import com.example.gfup2.exception.EntityException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -36,145 +38,110 @@ public class AlarmController {
 
     @PostMapping("/create")
     public ResponseEntity<?> alarmCreate(@RequestBody @Valid AlarmDto dt, BindingResult bindingResult){
-        try{
+        return AlarmControllerBase.run(() -> {
             CheckError.checkValidException(bindingResult);
-
-            Long dateTime = dt.getDatetime();
-            Boolean isRepeat = dt.getIsRepeat();
-            String name = dt.getName();
-            String message = dt.getMessage();
-            Alarm.Method method = dt.getMethod();
-            Boolean isActive = dt.getIsActive();
-            List<AlarmToDaysOfWeek> day = this.alarmToDaysOfWeekService
-                    .getAndAddAlarmToDaysOfWeekByDaysOfWeek(
-                            this.daysOfWeekService.dayOfWeekToDaysOfWeek(dt.getDay())
-                    );
-            User user = this.userService.getAuthUserFromSecurityContextHolder();
 
             return new ResponseEntity<AlarmIdDto>(
                     new AlarmIdDto(this.alarmService.create(
-                            dateTime,
-                            isRepeat,
-                            name,
-                            message,
-                            method,
-                            isActive,
-                            day,
-                            user
+                            dt.getDatetime(),
+                            dt.getIsRepeat(),
+                            dt.getName(),
+                            dt.getMessage(),
+                            dt.getMethod(),
+                            dt.getIsActive(),
+                            this.alarmToDaysOfWeekService
+                                    .getAndAddAlarmToDaysOfWeekByDaysOfWeek(
+                                            this.daysOfWeekService.dayOfWeekToDaysOfWeek(dt.getDay())
+                                    ),
+                            this.userService.getAuthUserFromSecurityContextHolder()
                     ).getId())
                     ,HttpStatus.OK
             );
-        }catch(VerifyException e){
-            return new ResponseEntity<String>(e.getMessage(), HttpStatus.UNAUTHORIZED);
-        }catch(ValidException e){
-            return new ResponseEntity<Map<String, String>>(e.getErrors(), HttpStatus.BAD_REQUEST);
-        }catch(Exception e){
-            return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
+        });
     }
 
     @GetMapping("/read")
     public ResponseEntity<?> alarmRead(@RequestParam(value="id", required = false) Long id, @RequestParam(value = "value", required = false) Integer value){
-        try{
+        return AlarmControllerBase.run(() -> {
             List<Alarm> alarms;
-            if (value == null) value = 1;
+            Integer v = value;
+            if (v == null) v = 1;
             if (id == null){
-                alarms = this.alarmService.read(this.userService.getAuthUserFromSecurityContextHolder(), value);
+                alarms = this.alarmService.read(this.userService.getAuthUserFromSecurityContextHolder(), v);
             }else{
-                alarms = this.alarmService.read(this.userService.getAuthUserFromSecurityContextHolder(),id, value);
+                alarms = this.alarmService.read(this.userService.getAuthUserFromSecurityContextHolder(),id, v);
             }
             List<AlarmDto> alarmDtos = new ArrayList<AlarmDto>();
 
             for(Alarm alarm: alarms){
-                Long alarmId = alarm.getId();
-                Long dateTime = Util.getLocalSecondsFromSqlTimeAndDate(alarm.getDate(), alarm.getTime());
-                Boolean isRepeat = alarm.getIsRepeat();
-                String name = alarm.getName();
-                String message = alarm.getMessage();
-                Alarm.Method method = alarm.getMethod();
-                Boolean isActive = alarm.getIsActive();
-                List<DaysOfWeek.DayOfWeek> days = this.daysOfWeekService
-                        .daysOfWeekToDayOfWeek(
-                                this.alarmToDaysOfWeekService.getDaysOfWeekToAlarmToDaysOfWeek(
-                                        alarm.getAlarmToDaysOfWeek()
-                                )
-                        );
-
                 AlarmDto alarmDto = new AlarmDto(
-                        alarmId,
-                        dateTime,
-                        isRepeat,
-                        name,
-                        message,
-                        method,
-                        isActive,
-                        days
+                        alarm.getId(),
+                        Util.getLocalSecondsFromSqlTimeAndDate(alarm.getDate(), alarm.getTime()),
+                        alarm.getIsRepeat(),
+                        alarm.getName(),
+                        alarm.getMessage(),
+                        alarm.getMethod(),
+                        alarm.getIsActive(),
+                        this.daysOfWeekService
+                                .daysOfWeekToDayOfWeek(
+                                        this.alarmToDaysOfWeekService.getDaysOfWeekToAlarmToDaysOfWeek(
+                                                alarm.getAlarmToDaysOfWeek()
+                                        )
+                                )
                 );
-
                 alarmDtos.add(alarmDto);
             }
 
             return new ResponseEntity<List<AlarmDto>>(alarmDtos, HttpStatus.OK);
-        }catch(VerifyException e){
-            return new ResponseEntity<String>(e.getMessage(), HttpStatus.UNAUTHORIZED);
-        }catch(Exception e){
-            return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
+        });
     }
 
     @PutMapping("/put")
     public ResponseEntity<?> alarmUpdate(@RequestBody @Valid AlarmDto dt, BindingResult bindingResult){
-        try{
+        return AlarmControllerBase.run(() -> {
             CheckError.checkValidException(bindingResult);
 
-            Long id = dt.getId();
-            Long dateTime = dt.getDatetime();
-            Boolean isRepeat = dt.getIsRepeat();
-            String name = dt.getName();
-            String message = dt.getMessage();
-            Alarm.Method method = dt.getMethod();
-            Boolean isActive = dt.getIsActive();
-            List<AlarmToDaysOfWeek> day = this.alarmToDaysOfWeekService
-                    .getAndAddAlarmToDaysOfWeekByDaysOfWeek(
-                            this.daysOfWeekService.dayOfWeekToDaysOfWeek(dt.getDay())
-                    );
-            User user = this.userService.getAuthUserFromSecurityContextHolder();
-
             this.alarmService.update(
-                    id,
-                    dateTime,
-                    isRepeat,
-                    name,
-                    message,
-                    method,
-                    isActive,
-                    day,
-                    user
+                    dt.getId(),
+                    dt.getDatetime(),
+                    dt.getIsRepeat(),
+                    dt.getName(),
+                    dt.getMessage(),
+                    dt.getMethod(),
+                    dt.getIsActive(),
+                    this.alarmToDaysOfWeekService
+                            .getAndAddAlarmToDaysOfWeekByDaysOfWeek(
+                                    this.daysOfWeekService.dayOfWeekToDaysOfWeek(dt.getDay())
+                            ),
+                    this.userService.getAuthUserFromSecurityContextHolder()
             );
 
             return new ResponseEntity<>(HttpStatus.OK);
-        }catch(VerifyException e){
-            return new ResponseEntity<String>(e.getMessage(), HttpStatus.UNAUTHORIZED);
-        }catch(ValidException e){
-            return new ResponseEntity<Map<String, String>>(e.getErrors(), HttpStatus.BAD_REQUEST);
-        }catch(Exception e){
-            return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
+        });
     }
 
     @DeleteMapping("/delete")
     public ResponseEntity<?> alarmDelete(@RequestBody @Valid AlarmIdDto dt, BindingResult bindingResult){
-        try{
-            CheckError.checkValidException(bindingResult);
-            this.alarmService.delete(this.userService.getAuthUserFromSecurityContextHolder(), dt.getId());
-
-            return new ResponseEntity<>(HttpStatus.OK);
-        }catch(VerifyException e){
-            return new ResponseEntity<String>(e.getMessage(), HttpStatus.UNAUTHORIZED);
-        }catch(ValidException e){
-            return new ResponseEntity<Map<String, String>>(e.getErrors(), HttpStatus.BAD_REQUEST);
-        }catch(Exception e){
-            return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
+        return AlarmControllerBase.run(() -> {
+                CheckError.checkValidException(bindingResult);
+                this.alarmService.delete(this.userService.getAuthUserFromSecurityContextHolder(), dt.getId());
+                return new ResponseEntity<>(HttpStatus.OK);
+        });
     }
+
+    private static class AlarmControllerBase {
+        public static ResponseEntity<?> run(ControllerBase base){
+            try{
+                return base.run();
+            }catch(VerifyException e){
+                return new ResponseEntity<String>(e.getMessage(), HttpStatus.UNAUTHORIZED);
+            }catch(ValidException e){
+                return new ResponseEntity<Map<String, String>>(e.getErrors(), HttpStatus.BAD_REQUEST);
+            }catch(Exception e){
+                return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+            }
+        }
+
+    }
+
 }
